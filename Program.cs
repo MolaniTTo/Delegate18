2c91097e23860e112d39f912e9c7f8cef2016d77
arnauitb <arnau.molano.7e7@itb.cat>
2024-03-13 12:32:41 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        List<int> numbers = new List<int> { 5, 2, 8, 1, 9 };
+
+       
+        Comparison<int> comparison = (x, y) =>
+        {
+           
+            if (x < y)
+                return 1;
+            else if (x > y)
+                return -1;
+            else
+                return 0;
+        };
+      
+        numbers.Sort(comparison);
+  
+        Console.WriteLine("Llista ordenada:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
